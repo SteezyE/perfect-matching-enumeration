193d3be82e1ddf997c6120d3674fd5e2a7741bbb
SteezyE <eliaspichottka@gmail.com>
2024-04-20 15:42:40 +0200

    added license notice

--- uno.c
+++ uno.c
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *                                                                         *
+ *  uno.c - enumerates perfect matchings in a bipartite graph              *
+ *  Copyright (C) 2024  SteezyE                                            *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "uno.h"
 
 void scc_iter(int * col_ids, int * col_ptrs, int * col_to_row, int * row_to_col, int * inactive,
--- uno.h
+++ uno.h
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *                                                                         *
+ *  uno.h - provides function and constant definitions for uno.c           *
+ *  Copyright (C) 2024  SteezyE                                            *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef __UNO_H
 #define __UNO_H
 
